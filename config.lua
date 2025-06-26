de7d7926bece1b957df12f68232e12f6c88e0422
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2025-06-25 21:52:49 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,25 +1,27 @@
 Config = {}
 
-Config.AdminRoleId = "YOUR_DISCORD_ADMIN_ROLE"
+-- Your admin role ID in Discord
+Config.AdminRoleId       = "YOUR_DISCORD_ADMIN_ROLE"
 
--- Set to true to enable park-anywhere functionality (Shift + F to park/unpark vehicles)
-Config.Parking = true
+-- Set to true to enable park-anywhere (Shift + F to park/unpark vehicles)
+Config.Parking           = true
 
--- Set to true to enable departments and department paychecks
-Config.Departments = true
+-- Enable departments & department paychecks
+Config.Departments       = true
 
--- How often to run distributePaychecks (in milliseconds)
-Config.paycheckInterval = 3600000  -- 1 hour
+-- How often to distribute paychecks (milliseconds)
+Config.paycheckInterval  = 3600000  -- 1 hour
 
--- Discord bot configuration
+-- ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+--┃       Discord Configuration Guide    ┃
+--┃  (Bot token & webhook live in       ┃
+--┃        server.lua – edit there)     ┃
+--┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 Config.Discord = {
-    -- The ID of the guild (server) you want to query
-    GuildId = "YOUR_DISCORD_GUILD_ID",
-    
-    -- NOTE: Bot token and webhook URL now live in server.lua
-    -- Edit your BotToken there:
-    --   BotToken = "YOUR_BOT_TOKEN"
-    --
-    -- And your WebhookURL there:
-    --   WebhookURL = "YOUR_DISCORD_WEBHOOK_URL"
+    -- Your Discord server (guild) ID
+    GuildId     = "YOUR_DISCORD_GUILD_ID",
+
+    -- BotToken & WebhookURL: see top of server.lua to configure
+    --   Config.Discord.BotToken   = "YOUR_BOT_TOKEN"
+    --   Config.Discord.WebhookURL = "YOUR_DISCORD_WEBHOOK_URL"
 }
